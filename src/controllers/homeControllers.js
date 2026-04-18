0abb7c8533ad17ae414d3253538857e8d4de577d
Kevin-Loran <kevinloran.eng@gmail.com>
2026-04-17 23:32:42 -0300

    feat: API_REST-configurando o 'sequelize' para a comunicação da API.

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,8 +1,16 @@
+import aluno from '../models/aluno';
+
 class Homecontrollrs {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await aluno.create({
+      nome: 'kevin',
+      sobrenome: 'loran',
+      email: 'kevitoloran@gmail.com',
+      idade: 122,
+      peso: 300,
+      altura: 2.50,
     });
+    res.json({ novoAluno });
   }
 }
 
