e42db1b4eb7b01cd6ccda4f6ae74d7d094728ed2
Kevin-Loran <kevinloran.eng@gmail.com>
2026-04-19 16:29:56 -0300

    feat: API_REST-adicionando um CRUD na feature -> usuario.

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,16 +1,84 @@
-import user from '../models/user';
+import User from '../models/user';
 
 class UserController {
   async store(req, res) {
     try {
-      const novouser = await user.create(req.body);
-      res.json({ novouser });
+      const novouser = await User.create(req.body);
+      return res.json(novouser);
     } catch (e) {
-      res.status(400).json({
-        errors: e.errors.map((err) => err.message),
+      return res.status(400).json({
+        errors: e.errors ? e.errors.map((err) => err.message) : [e.message],
       });
     }
   }
-}
 
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+}
 export default new UserController();
