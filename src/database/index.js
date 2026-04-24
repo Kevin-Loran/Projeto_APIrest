27cf6fcda48991efe01942ce7a5c9a69290ed8de
Kevin-Loran <kevinloran.eng@gmail.com>
2026-04-24 17:00:17 -0300

    feat: API_REST-Salvando as fotos com ID na base de dados.

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,11 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import aluno from '../models/aluno';
 import user from '../models/user';
+import Foto from '../models/Foto';
 
-const models = [aluno, user];
+const models = [aluno, user, Foto];
 
 const connection = new Sequelize(databaseConfig);
 
 models.forEach((model) => model.init(connection));
+models.forEach((model) => model.associate && model.associate(connection.models));
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-
+import loginRequired from '../middlewares/loginRequired';
 import fotoControllers from '../controllers/fotoControllers';
 
 const router = new Router();
 
-router.post('/', fotoControllers.store);
+router.post('/', loginRequired, fotoControllers.store);
 
 export default router;
