1007a33288d005d444e5da6d18d281008a2fe9fa
Kevin-Loran <kevinloran.eng@gmail.com>
2026-04-24 14:24:48 -0300

    feat: API_REST-Adicionando validação de arquivo.

--- src/controllers/fotoControllers.js
+++ src/controllers/fotoControllers.js
@@ -1,6 +1,19 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
+
 class Fotocontrollrs {
   async store(req, res) {
-    res.json('foto enviada');
+    return upload(req, res, (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import fotoControllers from '../controllers/fotoControllers';
-import multerConfig from '../config/multerConfig';
 
-const upload = multer(multerConfig);
 const router = new Router();
 
-router.post('/', upload.single('foto'), fotoControllers.store);
+router.post('/', fotoControllers.store);
 
 export default router;
